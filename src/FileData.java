cfcaafdcd1d730631021d953c2efa1b24e08aeb6
Owner <vitycity79@gmail.com>
2023-07-04 19:21:35 +0300

    equals&hash

--- src/FileData.java
+++ src/FileData.java
@@ -1,9 +1,11 @@
+import java.util.Objects;
+
 public class FileData {
     private final String name;
-    private final int size;
+    private final long size;
     private final String path;
 
-    public FileData(String name, int size, String path) {
+    public FileData(String name, long size, String path) {
         this.name = name;
         this.size = size;
         this.path = path;
@@ -18,6 +20,19 @@ public class FileData {
     }
     public String getPath() {
         return path;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FileData fileData = (FileData) o;
+        return size == fileData.size && Objects.equals(name, fileData.name) && Objects.equals(path, fileData.path);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, size, path);
     }
 
     @Override
